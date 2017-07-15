1c8b8622d91d2af05594ff34019028ccd4b3d392
vijaykumarjuwai <vijaykumar@juwai.com>
2017-07-15 15:50:16 +0800

    Fixed Route

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^about$', views.about),
+    url(r'^$', views.index, name='index'),
+    url(r'^about$', views.about, name='about'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
 ]
